f5ec07b7a9f9a1b7bfbaba789f04b6591248458c
Vijay <babarvm@gmail.com>
2020-11-01 00:19:25 +0530

    Add admin model.

---
+++ PTPMultiservice/Models/Admin.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace PTPMultiservice.Models
+{
+    [Table("admins")]
+    public class Admin
+    {
+        public int admin_id { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public DateTime created_on { get; set; }
+        public bool is_active { get; set; }
+    }
+}
